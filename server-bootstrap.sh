c579d13a1109f78a2c9c7cb5860fad54e65b1484
Fernando Reis Guimaraes <fernando.reis.guimaraes@gmail.com>
2026-02-24 02:29:48 -0300

    Rename backup of .bashrc in server-bootstrap.sh

---
+++ server-bootstrap.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# tailscale
+curl -fsSL https://tailscale.com/install.sh | sh && tailscale up --advertise-exit-node
+tailscale set --auto-update
+
+# ubuntu autoupgrade
+curl -fsSL 'https://raw.githubusercontent.com/ultra-mega-apps/public-bin/refs/heads/main/ubuntu-pro-unattended-upgrade.sh' | bash
+
+# ufw
+ufw --force reset
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow in on tailscale0
+ufw allow in on eth0 to any port 80 proto tcp
+ufw allow in on eth0 to any port 443 proto tcp
+ufw --force enable
+echo "try to connect to ssh over wan to test firewall"
+echo
+echo
+
+# node24
+curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
+
+# common apps
+apt update
+apt install -y \
+  nodejs \
+  gh \
+  rclone \
+  net-tools 
+
+# gh git config
+git config --global user.email "fernando@ultramega.email"
+git config --global user.name "Fernando Reis Guimaraes"
+gh auth login
+gh repo clone ultra-mega-apps/bin
+
+# .bashrc
+cp ~/.bashrc ~/.bashrc.before.server-bootstrap.bak
+sed -i 's/HISTSIZE=1000/HISTSIZE=10000/g' .bashrc
+sed -i 's/HISTFILESIZE=2000/HISTFILESIZE=20000/g' .bashrc
+sed -i 's/#force_color_prompt/force_color_prompt/g' .bashrc
+sed -i 's/01;32m/01;95m/g' .bashrc
+
+touch ~/.bash_aliases
+echo $'alias list=\'ls -lkhp\'' >> ~/.bash_aliases
+echo $'alias lista=\'ls -lkhap\'' >> ~/.bash_aliases
+
+echo >> .bashrc
+echo 'export PATH="~/bin:$PATH"' >> .bashrc
+
+source ~/.bashrc
